78a0d61c6ab9765efcf44e46519837dc11026843
Samuel Torris <samtorris@gmail.com>
2022-02-01 16:44:21 -0800

    Scaffolded the dbcontext from the db

---
+++ source/VotingApp/VotingApp/Models/CreatedVote.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace VotingApp.Models
+{
+    [Table("CreatedVote")]
+    public partial class CreatedVote
+    {
+        public CreatedVote()
+        {
+            Options = new HashSet<Option>();
+            SubmittedVotes = new HashSet<SubmittedVote>();
+        }
+
+        [Key]
+        [Column("ID")]
+        public int Id { get; set; }
+        [Column("UserID")]
+        public int? UserId { get; set; }
+        [StringLength(250)]
+        public string VoteDiscription { get; set; } = null!;
+        public bool Anonymous { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        [InverseProperty("CreatedVotes")]
+        public virtual User? User { get; set; }
+        [InverseProperty(nameof(Option.CreatedVote))]
+        public virtual ICollection<Option> Options { get; set; }
+        [InverseProperty(nameof(SubmittedVote.CreatedVote))]
+        public virtual ICollection<SubmittedVote> SubmittedVotes { get; set; }
+    }
+}
