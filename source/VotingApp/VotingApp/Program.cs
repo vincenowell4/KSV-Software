8ee1c4932d4f80a4d2e88ab0f7509e1115a00cd0
kaitlin32 <khaley18@mail.wou.edu>
2022-02-08 12:13:27 -0800

    va17 create a vote page finished (just needs testing and theme), also updated tasks on the user story md file

--- source/VotingApp/VotingApp/Program.cs
+++ source/VotingApp/VotingApp/Program.cs
@@ -1,7 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using VotingApp.DAL.Abstract;
+using VotingApp.DAL.Concrete;
+using VotingApp.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("VotingAppConnection");
+builder.Services.AddDbContext<VotingAppDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+builder.Services.AddScoped<DbContext, VotingAppDbContext>();
+builder.Services.AddScoped<ICreatedVoteRepository, CreatedVoteRepository>();
 
 var app = builder.Build();
 
