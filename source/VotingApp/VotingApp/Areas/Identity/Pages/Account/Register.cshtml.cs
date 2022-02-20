9c59a2f7f70ab58376a3cbad147a8480ba1f481b
Vince Nowell <vnowell21@wou.edu>
2022-02-19 21:29:47 -0800

    add bootstrap them and background images for pages

--- source/VotingApp/VotingApp/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ source/VotingApp/VotingApp/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -71,6 +71,14 @@ namespace VotingApp.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
+            [StringLength(100)]
+            [Display(Name = "First Name")]
+            public string FirstName { get; set; }
+
+            [StringLength(100)]
+            [Display(Name = "Last Name")]
+            public string LastName { get; set; }
+
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
