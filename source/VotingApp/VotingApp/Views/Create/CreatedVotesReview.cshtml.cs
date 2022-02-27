c98dbffb9032a5f44e0c8c9d86b421839f1f24ac
Vince Nowell <vnowell21@wou.edu>
2022-02-27 14:40:54 -0800

    add new files for vote review story

---
+++ source/VotingApp/VotingApp/Views/Create/CreatedVotesReview.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace VotingApp.Views.Create
+{
+    public class VoteReviewModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
