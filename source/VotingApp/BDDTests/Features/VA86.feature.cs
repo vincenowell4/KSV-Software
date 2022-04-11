1d8c59795031c7469423df33643315a918b07e73
Vince Nowell <vnowell21@wou.edu>
2022-04-11 06:53:22 -0700

    add rest of BDD tests

--- source/VotingApp/BDDTests/Features/VA86.feature.cs
+++ source/VotingApp/BDDTests/Features/VA86.feature.cs
@@ -100,10 +100,10 @@ the vote title, description and vote access code in the body of the email.", Pro
                         "LastName",
                         "Password"});
             table3.AddRow(new string[] {
-                        "sam2",
-                        "sam2@mail.com",
-                        "sam",
-                        "2",
+                        "vnowell21",
+                        "vnowell21@wou.edu",
+                        "Vince",
+                        "Nowell",
                         "Password123!"});
 #line 24
  testRunner.Given("the following user exists", ((string)(null)), table3, "Given ");
@@ -115,10 +115,10 @@ the vote title, description and vote access code in the body of the email.", Pro
                         "LastName",
                         "Password"});
             table4.AddRow(new string[] {
-                        "vnowell",
-                        "vnowell@example.com",
-                        "Vince",
-                        "Nowell",
+                        "pjonesl",
+                        "pjones@example.com",
+                        "Paul",
+                        "Jones",
                         "0a9dfi3.a"});
 #line 28
  testRunner.And("the following user does not exist", ((string)(null)), table4, "And ");
@@ -126,23 +126,16 @@ the vote title, description and vote access code in the body of the email.", Pro
         }
         
         [NUnit.Framework.TestAttribute()]
-        [NUnit.Framework.DescriptionAttribute("Non-logged in user can\'t create future vote time")]
-        [NUnit.Framework.CategoryAttribute("tag1")]
+        [NUnit.Framework.DescriptionAttribute("Logged in user will see Start Vote Now button and Start Vote Future button")]
         [NUnit.Framework.TestCaseAttribute("Vince", "Create", null)]
-        public virtual void Non_LoggedInUserCantCreateFutureVoteTime(string firstName, string page, string[] exampleTags)
+        public virtual void LoggedInUserWillSeeStartVoteNowButtonAndStartVoteFutureButton(string firstName, string page, string[] exampleTags)
         {
-            string[] @__tags = new string[] {
-                    "tag1"};
-            if ((exampleTags != null))
-            {
-                @__tags = System.Linq.Enumerable.ToArray(System.Linq.Enumerable.Concat(@__tags, exampleTags));
-            }
-            string[] tagsOfScenario = @__tags;
+            string[] tagsOfScenario = exampleTags;
             System.Collections.Specialized.OrderedDictionary argumentsOfScenario = new System.Collections.Specialized.OrderedDictionary();
             argumentsOfScenario.Add("FirstName", firstName);
             argumentsOfScenario.Add("Page", page);
-            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("Non-logged in user can\'t create future vote time", null, tagsOfScenario, argumentsOfScenario, this._featureTags);
-#line 35
+            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("Logged in user will see Start Vote Now button and Start Vote Future button", null, tagsOfScenario, argumentsOfScenario, this._featureTags);
+#line 33
 this.ScenarioInitialize(scenarioInfo);
 #line hidden
             bool isScenarioIgnored = default(bool);
@@ -165,17 +158,172 @@ this.ScenarioInitialize(scenarioInfo);
 #line 23
 this.FeatureBackground();
 #line hidden
-#line 36
- testRunner.Given("I am a user that isn\'t logged in", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
+#line 34
+ testRunner.Given("I am a user that is logged in", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
 #line hidden
-#line 37
+#line 35
  testRunner.When(string.Format("I navigate to \'{0}\' page", page), ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
 #line hidden
-#line 38
- testRunner.Then("I will not see the Start Immediate button", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+#line 36
+ testRunner.Then("I will see the Start Immediate Vote button", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
 #line hidden
-#line 39
-   testRunner.And("I will not see the Start Future Vote button", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 37
+   testRunner.And("I will see the Start Future Vote button", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line hidden
+            }
+            this.ScenarioCleanup();
+        }
+        
+        [NUnit.Framework.TestAttribute()]
+        [NUnit.Framework.DescriptionAttribute("Logged in user clicks Start Immediate Vote and Future DateTime is disabled")]
+        [NUnit.Framework.TestCaseAttribute("Vince", "Create", null)]
+        public virtual void LoggedInUserClicksStartImmediateVoteAndFutureDateTimeIsDisabled(string firstName, string page, string[] exampleTags)
+        {
+            string[] tagsOfScenario = exampleTags;
+            System.Collections.Specialized.OrderedDictionary argumentsOfScenario = new System.Collections.Specialized.OrderedDictionary();
+            argumentsOfScenario.Add("FirstName", firstName);
+            argumentsOfScenario.Add("Page", page);
+            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("Logged in user clicks Start Immediate Vote and Future DateTime is disabled", null, tagsOfScenario, argumentsOfScenario, this._featureTags);
+#line 42
+this.ScenarioInitialize(scenarioInfo);
+#line hidden
+            bool isScenarioIgnored = default(bool);
+            bool isFeatureIgnored = default(bool);
+            if ((tagsOfScenario != null))
+            {
+                isScenarioIgnored = tagsOfScenario.Where(__entry => __entry != null).Where(__entry => String.Equals(__entry, "ignore", StringComparison.CurrentCultureIgnoreCase)).Any();
+            }
+            if ((this._featureTags != null))
+            {
+                isFeatureIgnored = this._featureTags.Where(__entry => __entry != null).Where(__entry => String.Equals(__entry, "ignore", StringComparison.CurrentCultureIgnoreCase)).Any();
+            }
+            if ((isScenarioIgnored || isFeatureIgnored))
+            {
+                testRunner.SkipScenario();
+            }
+            else
+            {
+                this.ScenarioStart();
+#line 23
+this.FeatureBackground();
+#line hidden
+#line 43
+ testRunner.Given("I am a user that is logged in", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
+#line hidden
+#line 44
+ testRunner.When(string.Format("I navigate to \'{0}\' page", page), ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
+#line hidden
+#line 45
+   testRunner.And("I click on Start Immediate Vote", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line hidden
+#line 46
+ testRunner.Then("the Future DateTime textbox will be disabled", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+#line hidden
+            }
+            this.ScenarioCleanup();
+        }
+        
+        [NUnit.Framework.TestAttribute()]
+        [NUnit.Framework.DescriptionAttribute("Logged in user clicks Start Future Vote and Future DateTime is enabled")]
+        [NUnit.Framework.TestCaseAttribute("Vince", "Create", null)]
+        public virtual void LoggedInUserClicksStartFutureVoteAndFutureDateTimeIsEnabled(string firstName, string page, string[] exampleTags)
+        {
+            string[] tagsOfScenario = exampleTags;
+            System.Collections.Specialized.OrderedDictionary argumentsOfScenario = new System.Collections.Specialized.OrderedDictionary();
+            argumentsOfScenario.Add("FirstName", firstName);
+            argumentsOfScenario.Add("Page", page);
+            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("Logged in user clicks Start Future Vote and Future DateTime is enabled", null, tagsOfScenario, argumentsOfScenario, this._featureTags);
+#line 51
+this.ScenarioInitialize(scenarioInfo);
+#line hidden
+            bool isScenarioIgnored = default(bool);
+            bool isFeatureIgnored = default(bool);
+            if ((tagsOfScenario != null))
+            {
+                isScenarioIgnored = tagsOfScenario.Where(__entry => __entry != null).Where(__entry => String.Equals(__entry, "ignore", StringComparison.CurrentCultureIgnoreCase)).Any();
+            }
+            if ((this._featureTags != null))
+            {
+                isFeatureIgnored = this._featureTags.Where(__entry => __entry != null).Where(__entry => String.Equals(__entry, "ignore", StringComparison.CurrentCultureIgnoreCase)).Any();
+            }
+            if ((isScenarioIgnored || isFeatureIgnored))
+            {
+                testRunner.SkipScenario();
+            }
+            else
+            {
+                this.ScenarioStart();
+#line 23
+this.FeatureBackground();
+#line hidden
+#line 52
+ testRunner.Given("I am a user that is logged in", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
+#line hidden
+#line 53
+ testRunner.When(string.Format("I navigate to \'{0}\' page", page), ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
+#line hidden
+#line 54
+   testRunner.And("I click on Start Future Vote", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line hidden
+#line 55
+ testRunner.Then("the Future DateTime textbox will be enabled", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+#line hidden
+            }
+            this.ScenarioCleanup();
+        }
+        
+        [NUnit.Framework.TestAttribute()]
+        [NUnit.Framework.DescriptionAttribute("Logged in user enters a Future DateTime then clicks Start Immediate Vote and Futu" +
+            "re DateTime is blanked out")]
+        [NUnit.Framework.TestCaseAttribute("Vince", "Create", null)]
+        public virtual void LoggedInUserEntersAFutureDateTimeThenClicksStartImmediateVoteAndFutureDateTimeIsBlankedOut(string firstName, string page, string[] exampleTags)
+        {
+            string[] tagsOfScenario = exampleTags;
+            System.Collections.Specialized.OrderedDictionary argumentsOfScenario = new System.Collections.Specialized.OrderedDictionary();
+            argumentsOfScenario.Add("FirstName", firstName);
+            argumentsOfScenario.Add("Page", page);
+            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("Logged in user enters a Future DateTime then clicks Start Immediate Vote and Futu" +
+                    "re DateTime is blanked out", null, tagsOfScenario, argumentsOfScenario, this._featureTags);
+#line 60
+this.ScenarioInitialize(scenarioInfo);
+#line hidden
+            bool isScenarioIgnored = default(bool);
+            bool isFeatureIgnored = default(bool);
+            if ((tagsOfScenario != null))
+            {
+                isScenarioIgnored = tagsOfScenario.Where(__entry => __entry != null).Where(__entry => String.Equals(__entry, "ignore", StringComparison.CurrentCultureIgnoreCase)).Any();
+            }
+            if ((this._featureTags != null))
+            {
+                isFeatureIgnored = this._featureTags.Where(__entry => __entry != null).Where(__entry => String.Equals(__entry, "ignore", StringComparison.CurrentCultureIgnoreCase)).Any();
+            }
+            if ((isScenarioIgnored || isFeatureIgnored))
+            {
+                testRunner.SkipScenario();
+            }
+            else
+            {
+                this.ScenarioStart();
+#line 23
+this.FeatureBackground();
+#line hidden
+#line 61
+ testRunner.Given("I am a user that is logged in", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
+#line hidden
+#line 62
+ testRunner.When(string.Format("I navigate to \'{0}\' page", page), ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
+#line hidden
+#line 63
+   testRunner.And("I click on Start Future Vote", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line hidden
+#line 64
+   testRunner.And("I enter a Future Date", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line hidden
+#line 65
+   testRunner.And("I click on Start Immediate Vote", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line hidden
+#line 66
+ testRunner.Then("the Future DateTime textbox will be cleared", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
 #line hidden
             }
             this.ScenarioCleanup();
--- source/VotingApp/BDDTests/StepDefinitions/VA86StepDefinitions.cs
+++ source/VotingApp/BDDTests/StepDefinitions/VA86StepDefinitions.cs
@@ -13,51 +13,130 @@ namespace BDDTests.StepDefinitions
         // This is where we put data that is shared between steps in different files.
         private readonly ScenarioContext _scenarioContext;
         private readonly LoginPage _loginPage;
+        private readonly CreatePage _createPage;
         private readonly HomePage _homePage;
 
-        public VA86StepDefinitions(ScenarioContext context, LoginPage loginPage, HomePage homePage)
+        public VA86StepDefinitions(ScenarioContext context, LoginPage loginPage, HomePage homePage, CreatePage createPage)
         {
             _scenarioContext = context;
             _loginPage = loginPage;
             _homePage = homePage;
-        }
-
-
+            _createPage = createPage;
+         }
 
         [Given(@"the following user exists")]
-        public void GivenTheFollowingUserExists(Table table)
+        public void GivenTheFollowingUsersExist(Table table)
         {
-            throw new PendingStepException();
+            // Nothing to do for this step other than to save the background data
+            // that defines the users
+            IEnumerable<TestUser> users = table.CreateSet<TestUser>();
+            _scenarioContext["Users"] = users;
         }
 
         [Given(@"the following user does not exist")]
-        public void GivenTheFollowingUserDoesNotExist(Table table)
+        public void GivenTheFollowingUsersDoNotExist(Table table)
         {
-            throw new PendingStepException();
+            // Same with this one, just setting up the background data
+            IEnumerable<TestUser> nonUsers = table.CreateSet<TestUser>();
+            _scenarioContext["NonUsers"] = nonUsers;
         }
 
         [Given(@"I am a user that isn't logged in")]
         public void GivenIAmAUserThatIsntLoggedIn()
         {
-            throw new PendingStepException();
+            _homePage.Goto();
         }
 
         [When(@"I navigate to '([^']*)' page")]
         public void WhenINavigateToPage(string create)
         {
-            throw new PendingStepException();
+            _createPage.Goto();
         }
 
         [Then(@"I will not see the Start Immediate button")]
         public void ThenIWillNotSeeTheStartImmediateButton()
         {
-            throw new PendingStepException();
+            bool isVoteNowStartDisplayed = _createPage.GetVoteStartNowDisplayed;
+            isVoteNowStartDisplayed.Should().BeTrue();
         }
 
         [Then(@"I will not see the Start Future Vote button")]
         public void ThenIWillNotSeeTheStartFutureVoteButton()
         {
-            throw new PendingStepException();
+            bool isVoteNowFutureDisplayed = _createPage.GetVoteStartFutureDisplayed;
+            isVoteNowFutureDisplayed.Should().BeTrue();
+        }
+
+
+        [Given(@"I am a user that is logged in")]
+        public void GivenIAmAUserThatIsLoggedIn()
+        {
+            _loginPage.Goto();
+            IEnumerable<TestUser> users = (IEnumerable<TestUser>)_scenarioContext["Users"];
+            TestUser u = users.Where(u => u.FirstName == "Vince").FirstOrDefault();
+            _scenarioContext["CurrentUser"] = u;
+
+            _loginPage.EnterEmail(u.Email);
+            _loginPage.EnterPassword(u.Password);
+            _loginPage.Login();
+        }
+
+        //[Then(@"I will see that the Future DateTime textbox will be disabled")]
+        //public void ThenIWillSeeThatTheFutureDateTimeTextboxWillBeDisabled()
+        //{
+        //    throw new PendingStepException();
+        //}
+
+        [Then(@"I will see the Start Immediate Vote button")]
+        public void ThenIWillSeeTheStartImmediateVoteButton()
+        {
+            bool isVoteNowStartDisplayed = _createPage.GetVoteStartNowDisplayed;
+            isVoteNowStartDisplayed.Should().BeTrue();
+        }
+
+        [Then(@"I will see the Start Future Vote button")]
+        public void ThenIWillSeeTheStartFutureVoteButton()
+        {
+            bool isVoteFutureStartDisplayed = _createPage.GetVoteStartFutureDisplayed;
+            isVoteFutureStartDisplayed.Should().BeTrue();
+        }
+
+        [Then(@"the Future DateTime textbox will be enabled")]
+        public void ThenTheFutureDateTimeTextboxWillBeEnabled()
+        {
+            bool isVoteOpenDateTimeEnabled = _createPage.GetVoteOpenDateTimeEnabled;
+            isVoteOpenDateTimeEnabled.Should().BeTrue();
+        }
+
+        [When(@"I click on Start Immediate Vote")]
+        public void WhenIClickOnStartImmediateVote()
+        {
+            _createPage.StartVoteNowClick();
+        }
+
+        [When(@"I click on Start Future Vote")]
+        public void WhenIClickOnStartFutureVote()
+        {
+            _createPage.StartVoteFutureClick();
+        }
+
+        [When(@"I enter a Future Date")]
+        public void WhenIEnterAFutureDate()
+        {
+            _createPage.EnterFutureDate("2112-04-01 00:00:00");
+        }
+
+        [Then(@"the Future DateTime textbox will be cleared")]
+        public void ThenTheFutureDateTimeTextboxWillBeCleared()
+        {
+            _createPage.GetFutureDateText.Should().BeEmpty();
+        }
+
+    [Then(@"the Future DateTime textbox will be disabled")]
+        public void ThenTheFutureDateTimeTextboxWillBeDisabled()
+        {
+            bool isVoteOpenDateTimeEnabled = _createPage.GetVoteOpenDateTimeEnabled;
+            isVoteOpenDateTimeEnabled.Should().BeFalse();
         }
     }
 }
